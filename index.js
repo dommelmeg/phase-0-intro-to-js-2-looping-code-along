27e0767f629836e235a0cca0ecec94269dca4457
Megan <dommelmeg@gmail.com>
2022-10-27 22:03:39 -0400

    commit tree 96102cb017baa147ab8b2bd432782e61f1ffa4cc

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//     for (let i = 0; i < gifts.length; i++) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     }
+
+//     return gifts;
+// }
+
+// wrapGifts(gifts);
+
+const newArray = [];
+
+const writeCards = (names, events) => {
+    for (let i = 0; i < names.length; i++) {
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`);
+    }
+    return newArray;
+}
+
+const countDown = () => {
+    let i = 10;
+    while (i >= 0) {
+        console.log(i--);
+    }    
+}
